ea7f7627cb3c3fc823fac2004f679d65e3c41250
dfackler <dfackler@path2response.com>
2018-01-08 22:06:09 -0700

    added bitcoin picture

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 # display main page
 @app.route('/')
-def info():
-    return "Crypto Tool"
+def index():
+    return render_template('index.html')
+
+# display current bitcoin price
+@app.route('/bitcoin')
+def bitcoin():
+    return "Current bitcoin price"
 
 if __name__ == "__main__":
     app.run()
